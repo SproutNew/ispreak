9967191f7f3b343d23b0f47c8e787ba95ebe7832
SproutNew <627476366@qq.com>
2018-10-27 10:51:18 +0800

    10/27

--- app/src/main/java/com/example/asus/lism1/activity/MyActivity.java
+++ app/src/main/java/com/example/asus/lism1/activity/MyActivity.java
@@ -1,16 +1,29 @@
 package com.example.asus.lism1.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.asus.lism1.R;
 
 public class MyActivity extends Activity {
 
+    private TextView my4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        my4=findViewById(R.id.my4);
+        my4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(MyActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
